33ecb8335ae5dbed56fb296174f91d9a8677b5e2
Juan Cuervo <101907011+juanrcuervo@users.noreply.github.com>
2026-03-29 19:32:26 +0000

    Pipeline

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -4,4 +4,4 @@ import sys
 df = pd.DataFrame({"Terminal": [1, 2], "Passengers": [3, 4]})
 print(df.head())
 
-df.to_parquet(f"output_day_{sys.argv[1]}.parquet")
+# df.to_parquet(f"output_day_{sys.argv[1]}.parquet")
